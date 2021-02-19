dad0be859f19e57be1ff27386d0ef27ea12b5589
Franz Holzinger <holzinger@ttproducts.de>
2021-02-19 08:43:41 +0100

    remove outdated files

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-    'tx_mbiproductscategories_category' => 'JambageCom\\MbiProductsCategories\\Utility\\Category',
-    'tx_mbiproductscategories_treeview' => 'JambageCom\\MbiProductsCategories\\View\\TreeSelector',
-);
-
