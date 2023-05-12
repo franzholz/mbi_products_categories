f5f172ad3bb411c96cd3661edcd459860d85541b
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-12 10:15:38 +0200

    -

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,6 @@ defined('TYPO3') || die('Access denied.');
 
 call_user_func(function($extensionKey)
 {
-    $extensionConfiguration = [];
-    $originalConfiguration = [];
-
     $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
         \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
     )->get($extensionKey);
